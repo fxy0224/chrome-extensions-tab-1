3ac1c7726ae8858cb8a5fb1831ffba5ca6b96997
fxy <fxy17710117315@163.com>
2024-10-26 18:41:08 +0800

    modify background.js, manifest.json, make the default tab page of chrome is newpage.html

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
-chrome.action.onClicked.addListener(() => {
-    chrome.tabs.create({
-        url: "newpage.html"
-    });
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.tabs.create({ url: chrome.runtime.getURL("newpage.html") });
+});
+
+// 也可以在每次启动 Chrome 时打开
+chrome.runtime.onStartup.addListener(() => {
+    chrome.tabs.create({ url: chrome.runtime.getURL("newpage.html") });
 });
